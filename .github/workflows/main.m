52164f3e1af22bdcf27f78b0a8b2c11608fa972c
Orlando Carneiro <86997035+orliec@users.noreply.github.com>
2021-08-20 12:10:43 -0400

    Xcode has TVXML at main.m

---
+++ .github/workflows/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TVXML
+//
+//  Created by Orlando Carneiro on 8/18/21.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
